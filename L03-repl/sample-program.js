9b7358498f840cdd2c3faa61af13b8dda81e3c19
Jabi <michal.jabi@gmail.com>
2024-03-20 13:44:47 +0100

    lesson3(sample-program): how REPL extensions works?

---
+++ L03-repl/sample-program.js
@@ -0,0 +1,13 @@
+console.log("Hello World!", 20 + 120);
+
+console.log("Hello World!", 20 + 120);
+
+console.log("Hello World!?", 20 + 1);
+
+// throw new Error("Oh no!")
+
+console.log("Hello World!", 20 + 120);
+
+if( 20 || 10) {
+
+}
