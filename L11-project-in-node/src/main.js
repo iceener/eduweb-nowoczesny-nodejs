9500e8d8a86d2de8b3554dc6ad2d4d5b51cd8a2d
Jabi <michal.jabi@gmail.com>
2024-03-21 09:08:32 +0100

    lesson11(project): src and dist + package.json conventions.

---
+++ L11-project-in-node/src/main.js
@@ -0,0 +1,9 @@
+import {last} from "./array-helpers.js";
+
+console.log("Find last element program")
+
+const fruits = ["apples", "mangoes", "cherries"];
+
+console.log("My array is", fruits);
+
+console.log("Last element:", last(fruits))
