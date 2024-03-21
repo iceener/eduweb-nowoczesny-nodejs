ac27aa77119749f09edf7ea24c45d45d400083fc
Jabi <michal.jabi@gmail.com>
2024-03-21 09:53:30 +0100

    lesson12(lodash): Install lib from public repository

---
+++ L12-install-libs/src/main.js
@@ -0,0 +1,9 @@
+const lo = require("lodash")
+
+console.log("Find last element program")
+
+const fruits = ["apples", "mangoes", "cherries"];
+
+console.log("My array is", fruits);
+
+console.log("Last element:", lo.last(fruits))
