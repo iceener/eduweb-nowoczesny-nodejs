35269a8b75b7a3a20cf47226cfb75389605a966e
Jabi <michal.jabi@gmail.com>
2024-03-27 11:28:15 +0100

    lesson39(read-my-file): proper path join

--- L39-proper-path-resolution/src/read-my-file.mjs
+++ L39-proper-path-resolution/src/read-my-file.mjs
@@ -1,7 +1,12 @@
 import fsp from 'node:fs/promises'
+import { fileURLToPath } from "node:url"
+import path from "node:path";
 
 try {
-  const data = await fsp.readFile('./vegetables.txt', 'utf-8')
+  const data = await fsp.readFile(path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    'vegetables.txt'
+  ), 'utf-8')
   console.log(data)
 } catch (e) {
   console.error(e);
